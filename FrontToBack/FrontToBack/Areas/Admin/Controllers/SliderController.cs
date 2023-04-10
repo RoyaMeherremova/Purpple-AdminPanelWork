055a6a098b2a3639a7edf54ed0f8b5cb3cc6d289
unknown <royaam@code.edu.az>
2023-04-11 01:47:34 +0400

    task are ready

---
+++ FrontToBack/FrontToBack/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,58 @@
+﻿using FrontToBack.Data;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBack.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+        
+        private readonly AppDbContext _context;
+
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            IEnumerable<SliderImage> slider = await _context.SliderImages.Where(m => !m.SoftDeleted).ToListAsync();
+            return View(slider);
+        }
+        public async Task<IActionResult> Detail(int? id)
+        {
+            //eyer kimse urlden UI-dan yani  id--ni silse seyfe baglansin
+            //BadRequest=Exception cixaririq
+            if (id == null) return BadRequest();
+            SliderImage? slider = await _context.SliderImages.FirstOrDefaultAsync(m => m.Id == id);
+            //eyer kimse sehv regem yazibsa Url-e
+            //NotFound-tapilmadi deye Exception 
+            if (slider == null) return NotFound();
+            return View(slider);
+        }
+
+
+        //[HttpGet]-datani goturende 
+        [HttpGet]
+
+        public IActionResult Create()    /*async-elemirik cunku data gelmir databazadan*/
+        {
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return BadRequest();
+            SliderImage? slider = await _context.SliderImages.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (slider == null) return NotFound();
+            return View(slider);
+
+        }
+    }
+}
---
+++ FrontToBack/FrontToBack/Areas/Admin/Controllers/WorkController.cs
@@ -0,0 +1,58 @@
+﻿using FrontToBack.Data;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBack.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WorkController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public WorkController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            IEnumerable<WorkImage> works = await _context.WorkImages.Where(m => !m.SoftDeleted).ToListAsync();
+            return View(works);
+        }
+
+
+
+         public async Task<IActionResult> Detail(int? id)
+        {
+            
+            if (id == null) return BadRequest();
+            WorkImage? work = await _context.WorkImages.Include(m => m.Work).FirstOrDefaultAsync(m => m.Id == id);
+        
+            if (work == null) return NotFound();
+            return View(work);
+        }
+
+
+        //[HttpGet]-datani goturende 
+        [HttpGet]
+
+        public IActionResult Create()    
+        {
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return BadRequest();
+            WorkImage? work = await _context.WorkImages.Include(m=>m.Work).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (work == null) return NotFound();
+            return View(work);
+
+        }
+    }
+}
